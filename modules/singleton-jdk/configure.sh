145fcd13aa67452714abd6b56859ccfff4c5c829
Ruben Novelli <31356779+rdnovell@users.noreply.github.com>
2023-06-21 11:44:34 +0200

    Improve clean up java packages

--- modules/singleton-jdk/configure.sh
+++ modules/singleton-jdk/configure.sh
@@ -13,4 +13,8 @@ fi
 
 # Clean up any java-* packages that have been installed that do not match
 # our stated JAVA_VERSION-JAVA_VENDOR (e.g.: 11-openjdk; 1.8.0-openj9)
-rpm -e --nodeps $(rpm -qa java-* | grep -v "^java-${JAVA_VERSION}-${JAVA_VENDOR}")
+rpm -qa java-\* | while read pkg; do
+    if ! echo "$pkg" | grep -q "^java-${JAVA_VERSION}-${JAVA_VENDOR}"; then
+        rpm -e --nodeps "$pkg"
+    fi
+done
